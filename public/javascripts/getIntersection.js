b49da2df381a94d276a2512b6dbcf603f3327375
Nurazem Kaldybaev <nurazemkg@gmail.com>
2015-02-12 21:31:22 +0000

    Refactor getIntersection and getTimeline js

--- public/javascripts/getIntersection.js
+++ public/javascripts/getIntersection.js
@@ -1,25 +1,44 @@
-$(document).ready(function(){
-    $("#getIntersection").click(function(e){
-        e.preventDefault();
-        $('#intersectionTable > tbody').empty();
-        $('#timelineTable').addClass('hidden');
-        $.ajax({
+;(function($) {
+    var init = function() {
+        $("#getIntersection").click(function(e){
+            e.preventDefault();
+            $('#intersectionTable > tbody').empty();
+            $('#timelineTable').addClass('hidden');
+            $('#username').val('');
+            fetchIntersection();
+        });
+    };
+
+    var makePath = function() {
+        return "/intersection/" + $("#firstUser").val() + "/" + $("#secondUser").val();
+    };
+
+    var displayIntersection = function(following_users) {
+        var rows = '';
+        following_users.forEach(function(tweet) {
+            var columns = '';
+            columns += '<td>' + tweet['screen_name'] + '</td>';
+            columns += '<td>' + tweet['name'] + '</td>';
+            columns += '<td>' + tweet['description'] + '</td>';
+            columns += '<td>' + tweet['location'] + '</td>';
+            rows += '<tr>' + columns + '</tr>';
+        });
+        $('#intersectionTable tbody').append(rows);
+        $('#intersectionTable').removeClass('hidden');
+    };
+
+    var fetchIntersection = function() {
+        return $.ajax({
             type: "GET",
-            url: "/intersection/" + $("#firstUser").val() + "/" + $("#secondUser").val(),
+            url: makePath(),
             success: function(response) {
-                console.log(response);
-                var rows = '';
-                response[0].forEach(function(tweet) {
-                    var columns = '';
-                    columns += '<td>' + tweet['screen_name'] + '</td>';
-                    columns += '<td>' + tweet['name'] + '</td>';
-                    columns += '<td>' + tweet['description'] + '</td>';
-                    columns += '<td>' + tweet['location'] + '</td>';
-                    rows += '<tr>' + columns + '</tr>';
-                });
-                $('#intersectionTable tbody').append(rows);
-                $('#intersectionTable').removeClass('hidden');
+                displayIntersection(response[0]);
+            },
+            error: function(e) {
+                alert(e.responseJSON.error);
             }
         });
-    });
-});
+    };
+
+    $(init);
+})(jQuery);
--- public/javascripts/getTimeline.js
+++ public/javascripts/getTimeline.js
@@ -1,23 +1,43 @@
-$(document).ready(function(){
-    $("#getTimeline").click(function(e){
-        e.preventDefault();
-        $('#timelineTable > tbody').empty();
-        $('#intersectionTable').addClass('hidden');
+;(function($) {
+    var init = function() {
+        $("#getTimeline").click(function(e){
+            e.preventDefault();
+            $('#timelineTable > tbody').empty();
+            $('#intersectionTable').addClass('hidden');
+            $('#firstUser').val('');
+            $('#secondUser').val('');
+            fetchTimeline();
+        });
+    };
 
-        $.ajax({
+    var makePath = function() {
+        return "/timeline/" + $("#username").val();
+    };
+
+    var displayTimeline = function(tweets) {
+        var rows = '';
+        tweets.forEach(function(tweet) {
+            var columns = '';
+            columns += '<td>' + tweet['text'] + '</td>';
+            columns += '<td>' + tweet['created_at'] + '</td>';
+            rows += '<tr>' + columns + '</tr>';
+        });
+        $('#timelineTable tbody').append(rows);
+        $('#timelineTable').removeClass('hidden');
+    };
+
+    var fetchTimeline = function() {
+        return $.ajax({
             type: "GET",
-            url: "/timeline/" + $("#username").val(),
+            url: makePath(),
             success: function(response) {
-                var rows = '';
-                response.forEach(function(tweet) {
-                    var columns = '';
-                    columns += '<td>' + tweet['text'] + '</td>';
-                    columns += '<td>' + tweet['created_at'] + '</td>';
-                    rows += '<tr>' + columns + '</tr>';
-                });
-                $('#timelineTable tbody').append(rows);
-                $('#timelineTable').removeClass('hidden');
+                displayTimeline(response);
+            },
+            error: function(e) {
+                alert(e.responseJSON.error);
             }
         });
-    });
-});
+    };
+
+    $(init);
+})(jQuery);
